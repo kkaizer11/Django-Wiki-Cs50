bde267dba13af9779716617e22131f81a17161c4
kkaizer11 <kaizerin.m@gmail.com>
2023-08-29 15:12:24 -0300

    entry feito vá para o proximo item

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
 from . import util
+from markdown2 import markdown
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
+
+def wiki_Entry(request, title):
+    entry = util.get_entry(title)
+    if entry is None:
+        return render(request, "encyclopedia/error.html")
+    else:
+        entry_content = markdown(entry)
+        content = {
+            "title": title,
+            "entry_md": entry_content,
+        }
+        return render(request, "encyclopedia/entries.html", content)
