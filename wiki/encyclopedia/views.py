6abb43dca761752cb837d568ae9ce46e1a43770f
kkaizer11 <kaizerin.m@gmail.com>
2023-08-30 21:15:01 -0300

    search in progress

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 from markdown2 import markdown
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
+    content = {
+        "entries": util.list_entries(),
+    }
+    return render(request, "encyclopedia/index.html", content)
 
 
 def wiki_Entry(request, title):
@@ -19,3 +22,20 @@ def wiki_Entry(request, title):
             "entry_md": entry_content,
         }
         return render(request, "encyclopedia/entries.html", content)
+
+
+def search(request):
+    query = request.POST["query"]
+    ls_entries = util.list_entries()
+    result = []
+    for i in range(len(ls_entries)):
+        if query.lower() == ls_entries[i].lower():
+            return redirect("wiki_Entry", title=ls_entries[i])
+        elif query.lower() in ls_entries[i].lower():
+            result.append(ls_entries[i])
+    content = {
+        "title": "Search results",
+        "query": query,
+        "feedback": result,
+    }
+    return render(request, "encyclopedia/search.html", content)
